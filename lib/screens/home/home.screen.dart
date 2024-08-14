3b264d1f0afb3d36653a8631f111dcaf2aadc6cf
Harsh <harsh.vipinkp@gmail.com>
2024-08-14 22:44:10 +0530

    UI update: Replaced FAB with two new FABs for income and expense

--- lib/screens/home/home.screen.dart
+++ lib/screens/home/home.screen.dart
@@ -15,7 +15,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:intl/intl.dart';
 
-
 String greeting() {
   var hour = DateTime.now().hour;
   if (hour < 12) {
@@ -26,7 +25,6 @@ String greeting() {
   }
   return 'Evening';
 }
-
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -47,24 +45,24 @@ class _HomeScreenState extends State<HomeScreen> {
   double _expense = 0;
   //double _savings = 0;
   DateTimeRange _range = DateTimeRange(
-      start: DateTime.now().subtract(Duration(days: DateTime.now().day -1)),
-      end: DateTime.now()
-  );
+      start: DateTime.now().subtract(Duration(days: DateTime.now().day - 1)),
+      end: DateTime.now());
   Account? _account;
   Category? _category;
 
   void openAddPaymentPage(PaymentType type) async {
-    Navigator.of(context).push(MaterialPageRoute(builder: (builder)=>PaymentForm(type: type)));
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (builder) => PaymentForm(type: type)));
   }
 
-  void handleChooseDateRange() async{
+  void handleChooseDateRange() async {
     final selected = await showDateRangePicker(
       context: context,
       initialDateRange: _range,
       firstDate: DateTime(2019),
       lastDate: DateTime.now(),
     );
-    if(selected != null) {
+    if (selected != null) {
       setState(() {
         _range = selected;
         _fetchTransactions();
@@ -73,12 +71,13 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void _fetchTransactions() async {
-    List<Payment> trans = await _paymentDao.find(range: _range, category: _category, account:_account);
+    List<Payment> trans = await _paymentDao.find(
+        range: _range, category: _category, account: _account);
     double income = 0;
     double expense = 0;
     for (var payment in trans) {
-      if(payment.type == PaymentType.credit) income += payment.amount;
-      if(payment.type == PaymentType.debit) expense += payment.amount;
+      if (payment.type == PaymentType.credit) income += payment.amount;
+      if (payment.type == PaymentType.debit) expense += payment.amount;
     }
 
     //fetch accounts
@@ -92,23 +91,22 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-
   @override
   void initState() {
     super.initState();
     _fetchTransactions();
 
-    _accountEventListener = globalEvent.on("account_update", (data){
+    _accountEventListener = globalEvent.on("account_update", (data) {
       debugPrint("accounts are changed");
       _fetchTransactions();
     });
 
-    _categoryEventListener = globalEvent.on("category_update", (data){
+    _categoryEventListener = globalEvent.on("category_update", (data) {
       debugPrint("categories are changed");
       _fetchTransactions();
     });
 
-    _paymentEventListener = globalEvent.on("payment_update", (data){
+    _paymentEventListener = globalEvent.on("payment_update", (data) {
       debugPrint("payments are changed");
       _fetchTransactions();
     });
@@ -121,6 +119,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _paymentEventListener?.cancel();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -135,142 +134,188 @@ class _HomeScreenState extends State<HomeScreen> {
       // ),
       body: SingleChildScrollView(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 60),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin:
+                const EdgeInsets.only(left: 15, right: 15, bottom: 15, top: 60),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Hi! Good ${greeting()}"),
+                BlocConsumer<AppCubit, AppState>(
+                    listener: (context, state) {},
+                    builder: (context, state) => Text(
+                          state.username ?? "Guest",
+                          style: const TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ))
+              ],
+            ),
+          ),
+          AccountsSlider(
+            accounts: _accounts,
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(children: [
+              const Text("Payments",
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17)),
+              const Expanded(child: SizedBox()),
+              MaterialButton(
+                onPressed: () {
+                  handleChooseDateRange();
+                },
+                height: double.minPositive,
+                padding: EdgeInsets.zero,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                child: Row(
                   children: [
-                    Text("Hi! Good ${greeting()}"),
-                    BlocConsumer<AppCubit, AppState>(
-                        listener: (context, state){
-
-                        },
-                        builder: (context, state)=>Text(state.username ?? "Guest", style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),)
-                    )
+                    Text(
+                      "${DateFormat("dd MMM").format(_range.start)} - ${DateFormat("dd MMM").format(_range.end)}",
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    const Icon(Icons.arrow_drop_down_outlined)
                   ],
                 ),
               ),
-              AccountsSlider(accounts: _accounts,),
-              const SizedBox(height: 15,),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                    children: [
-                      const Text("Payments", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 17)),
-                      const Expanded(child: SizedBox()),
-                      MaterialButton(
-                        onPressed: (){
-                          handleChooseDateRange();
-                        },
-                        height: double.minPositive,
-                        padding: EdgeInsets.zero,
-                        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                        child: Row(
-                          children: [
-                            Text("${DateFormat("dd MMM").format(_range.start)} - ${DateFormat("dd MMM").format(_range.end)}", style: Theme.of(context).textTheme.bodySmall,),
-                            const Icon(Icons.arrow_drop_down_outlined)
-                          ],
+            ]),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 14, horizontal: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          color: ThemeColors.success.withOpacity(0.2),
                         ),
-                      ),
-                    ]
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Expanded(
-                        child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: ThemeColors.success.withOpacity(0.2),
-                            ),
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text.rich(
-                                      TextSpan(
-                                          children: [
-                                            //TextSpan(text: "▼", style: TextStyle(color: ThemeColors.success)),
-                                            TextSpan(text:"Income", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600)),
-                                          ]
-                                      )
-                                  ),
-                                  const SizedBox(height: 5,),
-                                  CurrencyText(_income, style:  const TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: ThemeColors.success),)
-                                ],
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text.rich(TextSpan(children: [
+                                //TextSpan(text: "▼", style: TextStyle(color: ThemeColors.success)),
+                                TextSpan(
+                                    text: "Income",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                              ])),
+                              const SizedBox(
+                                height: 5,
                               ),
-                            )
-                        )
-                    ),
-                    const SizedBox(width: 10,),
-                    Expanded(
-                        child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: ThemeColors.error.withOpacity(0.2),
-                            ),
-                            child: SizedBox(
-                              width: double.infinity,
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  const Text.rich(
-                                      TextSpan(
-                                          children: [
-                                            //TextSpan(text: "▲", style: TextStyle(color: ThemeColors.error)),
-                                            TextSpan(text:"Expense", style: TextStyle(fontSize: 12, fontWeight: FontWeight.w600)),
-                                          ]
-                                      )
-                                  ),
-                                  const SizedBox(height: 5,),
-                                  CurrencyText(_expense, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: ThemeColors.error),)
-                                ],
-                              ),
-                            )
-                        )
-                    ),
-                  ],
+                              CurrencyText(
+                                _income,
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: ThemeColors.success),
+                              )
+                            ],
+                          ),
+                        ))),
+                const SizedBox(
+                  width: 10,
                 ),
-              ),
-              _payments.isNotEmpty? ListView.separated(
-                padding:  EdgeInsets.zero,
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (BuildContext context, index){
-                  return PaymentListItem(payment: _payments[index], onTap: (){
-                    Navigator.of(context).push(MaterialPageRoute(builder: (builder)=>PaymentForm(type: _payments[index].type, payment: _payments[index],)));
-                  });
-
-                },
-                separatorBuilder: (BuildContext context, int index){
-                  return Container(
-                    width: double.infinity,
-                    color: Colors.grey.withAlpha(25),
-                    height: 1,
-                    margin: const EdgeInsets.only(left: 75, right: 20),
-                  );
-                },
-                itemCount: _payments.length,
-              ):Container(
-                padding: const EdgeInsets.symmetric(vertical: 25),
-                alignment: Alignment.center,
-                child: const Text("No payments!"),
-              ),
-            ],
-          )
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()=> openAddPaymentPage(PaymentType.credit),
-        child: const Icon(Icons.add),
+                Expanded(
+                    child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 14, horizontal: 16),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          color: ThemeColors.error.withOpacity(0.2),
+                        ),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text.rich(TextSpan(children: [
+                                //TextSpan(text: "▲", style: TextStyle(color: ThemeColors.error)),
+                                TextSpan(
+                                    text: "Expense",
+                                    style: TextStyle(
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.w600)),
+                              ])),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              CurrencyText(
+                                _expense,
+                                style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                    color: ThemeColors.error),
+                              )
+                            ],
+                          ),
+                        ))),
+              ],
+            ),
+          ),
+          _payments.isNotEmpty
+              ? ListView.separated(
+                  padding: EdgeInsets.zero,
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (BuildContext context, index) {
+                    return PaymentListItem(
+                        payment: _payments[index],
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (builder) => PaymentForm(
+                                    type: _payments[index].type,
+                                    payment: _payments[index],
+                                  )));
+                        });
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return Container(
+                      width: double.infinity,
+                      color: Colors.grey.withAlpha(25),
+                      height: 1,
+                      margin: const EdgeInsets.only(left: 75, right: 20),
+                    );
+                  },
+                  itemCount: _payments.length,
+                )
+              : Container(
+                  padding: const EdgeInsets.symmetric(vertical: 25),
+                  alignment: Alignment.center,
+                  child: const Text("No payments!"),
+                ),
+        ],
+      )),
+          /**
+           * Buttons to add income and expense
+           */
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(width: 16.0),
+          FloatingActionButton(
+            onPressed: () => openAddPaymentPage(PaymentType.credit),
+            backgroundColor: ThemeColors.success,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(width: 10.0),
+          FloatingActionButton(
+            onPressed: () => openAddPaymentPage(PaymentType.debit),
+            backgroundColor: ThemeColors.error,
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
